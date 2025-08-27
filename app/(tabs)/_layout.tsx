3f32dd1a785a774c6b750c6200a2071ac2910ff9
bkjonathan <kyawhtetsan.jar1990@gmail.com>
2025-08-27 11:46:16 +0700

    Refactor Tabs layout and update `.prettierignore` file

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import {Tabs} from "expo-router";
-import {Ionicons} from "@expo/vector-icons";
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 const TabsLayout = () => {
   return (
-    <Tabs screenOptions={{headerShown: false}}>
-      <Tabs.Screen name="index" options={{title: "Todos",tabBarIcon:({color,size})=>  <Ionicons name="flash-outline" size={size} color={color} />}}/>
-      <Tabs.Screen name="settings" options={{title: "Settings",tabBarIcon:({color,size})=>  <Ionicons name="settings-outline" size={size} color={color} />}}/>
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Todos',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
 
-export default TabsLayout
+export default TabsLayout;
